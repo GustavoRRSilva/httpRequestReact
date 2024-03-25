fb0bfa3e20845d2604325ed4b23a372a028e18cf
Gustavo <gstvrrds@gmail.com>
2024-03-25 15:17:51 -0300

    feat:first Commit class: refatoring post

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useFetch } from "./hooks/useFetch";
+
+function App() {
+  const [products, setProducts] = useState([]);
+  const url = "http://localhost:3000/products";
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const { data: items, httpConfig } = useFetch(url);
+
+  //Enviando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fetchData();
+  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const product = {
+      name,
+      price,
+    };
+
+    /*
+    const res = await fetch(url, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+    const newProduct = await res.json();
+    setProducts([...products, newProduct]);
+
+    
+    */
+    httpConfig(product,"POST")
+    //5 refatorando post
+  };
+  return (
+    <>
+      <div className="">
+        <h2>Lista de produtos:</h2>
+        <ul>
+          {items &&
+            items.map((product) => (
+              <li key={product.id}>
+                {product.name} - R$:{product.price}
+              </li>
+            ))}
+        </ul>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Price:
+            <input
+              type="number"
+              price="price"
+              id="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Enviar dados" />
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  //Refatoring post
+
+  const [config, setConfig] = useState(null);
+  const [method, setMethod] = useState(null);
+  const [callFetch, setCallFetch] = useState(false);
+
+  const httpConfig = (data, method) => {
+    if (method === "POST") {
+      setConfig({
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      setMethod("POST");
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url);
+      const json = await res.json();
+      setData(json);
+    };
+    fetchData();
+  }, [url, callFetch]);
+
+  //Refatoring post
+  useEffect(() => {
+    const httpRequest = async () => {
+      if (method === "POST") {
+        let fetchOptions = [url, config];
+        let res = await fetch(...fetchOptions);
+        const json = await res.json();
+        setCallFetch(json);
+      }
+    };
+    httpRequest();
+  }, [config]);
+
+  return { data, httpConfig };
+};
